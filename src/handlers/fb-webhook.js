66750840eba0d07f45343217b60d3bd6ce810501
Naufal Prima Yoriko <primayoriko@gmail.com>
2022-07-02 09:27:12 +0700

    fix: error in handling webhook request

--- src/handlers/fb-webhook.js
+++ src/handlers/fb-webhook.js
@@ -4,16 +4,15 @@ const
 function verifyWebhook(req, res) {
 	const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
-	let mode = req.query["hub.mode"];
-	let token = req.query["hub.verify_token"];
-	let challenge = req.query["hub.challenge"];
+	let 
+		mode = req.query["hub.mode"],
+		token = req.query["hub.verify_token"],
+		challenge = req.query["hub.challenge"];
 
 	if (mode && token) {
-
 		if (mode === "subscribe" && token === VERIFY_TOKEN) {
 			// console.log("WEBHOOK_VERIFIED");
 			res.status(200).send(challenge);
-
 		} else {
 			res.sendStatus(403);
 		}
@@ -24,27 +23,29 @@ async function handleWebhook(req, res) {
 	let body = req.body;
 
 	if (body.object === "page") {
+		body.entry.forEach(entry => {
+			let 
+				webhookEvent = entry.messaging[0],
+				senderPsid = webhookEvent.sender.id,
+				validEvent = false,
+				response = {};
 
-		body.entry.forEach(function(entry) {
-
-			let webhookEvent = entry.messaging[0];
-			// console.log(webhookEvent);
-
-			let senderPsid = webhookEvent.sender.id;
-
-			let response;
-			// console.log("Sender PSID: " + senderPsid);
-			let validEvent = false;
-
-			if (webhookEvent.message) {
+			if (webhookEvent.message && webhookEvent.message.text) {
 				const messageText = webhookEvent.message.text;
 				const regex = /^\d{4}-\d{2}-\d{2}$/;
 
 				if (messageText === "Hi") {
-					response = "What is your first name?";
+					response.text = "What is your first name?";
 				} else if (messageText.match(regex) === null) {
-					response = "When is your birthday? please input in YYYY-MM-DD format";
+					response.text = "When is your birthday? please input in YYYY-MM-DD format";
 				} else {
+					const positive_response_payload = JSON.stringify({
+						"findNextDate": true,
+						"date": messageText
+					});
+					const negative_response_payload = JSON.stringify({
+						"findNextDate": false,
+					});
 					response = {
 						"attachment": {
 							"type": "template",
@@ -52,23 +53,18 @@ async function handleWebhook(req, res) {
 								"template_type": "generic",
 								"elements": [{
 									"title": `Your Birthday is on ${messageText}`,
-									"subtitle": "Find your next birthday date?",
+									"subtitle": "Find your how long until your next birthday date?",
 									// "image_url": attachmentUrl,
 									"buttons": [
 										{
 											"type": "postback",
 											"title": "yes",
-											"payload": {
-												"findNextDate": true,
-												"date": messageText
-											},
+											"payload": positive_response_payload,
 										},
 										{
 											"type": "postback",
 											"title": "no",
-											"payload": {
-												"findNextDate": false,
-											},
+											"payload": negative_response_payload,
 										}
 									],
 								}]
@@ -77,39 +73,43 @@ async function handleWebhook(req, res) {
 					};
 				}
 
-				// handleMessage(senderPsid, webhookEvent.message);
 				validEvent = true;
+
 			} else if (webhookEvent.postback) {
 				const payload = JSON.parse(webhookEvent.postback.payload);
 				
 				if (!payload.findNextDate) {
-					response = "Goodbye👋";
+					response.text = "Goodbye👋";
+					
 				} else {
-					const dateArr = payload.date
-						.split("-")
-						.map(num => parseInt(num));
-					let birthDate = new Date(dateArr[2], dateArr[1] - 1, dateArr[0]);
-					let currDate = new Date();
+					const
+						currDate = new Date(), 
+						dateArr = payload.date
+							.split("-")
+							.map(num => parseInt(num));
+
+					let birthDate = new Date(currDate.getFullYear(), dateArr[1] - 1, dateArr[2]);
 					
 					if (birthDate < currDate) {
-						birthDate = new Date(dateArr[2] + 1, dateArr[1] - 1, dateArr[0]);
+						birthDate = new Date(currDate.getFullYear() + 1, dateArr[1] - 1, dateArr[2]);
 					}
 
 					const daysDiff = Math.ceil((birthDate - currDate)/(1000*60*60*24));
 					
-					response = `There are ${daysDiff} days left until your next birthday`;
+					response.text = `There are ${daysDiff} days left until your next birthday`;
 				}
-				// handlePostback(senderPsid, webhookEvent.postback);
+				
 				validEvent = true;
 			}
 
 			if (validEvent) {
-				sendReply(senderPsid, response);
-				res.status(200).send("EVENT_RECEIVED");
-				return;
+				sendReply(senderPsid, response)
+					.catch(err => {
+						console.log(err);
+					});
 			}
 		});
-
+		res.status(200).send("EVENT_RECEIVED");
 	} else {
 		res.sendStatus(404);
 	}
--- src/services/fb-messenger.js
+++ src/services/fb-messenger.js
@@ -1,97 +1,5 @@
 const
 	axios = require("axios").default;
-	// querystring = require("querystring");
-
-// function handleMessage(senderPsid, receivedMessage) {
-// 	let response;
-
-// 	// Checks if the message contains text
-// 	if (receivedMessage.text) {
-// 		// Create the payload for a basic text message, which
-// 		// will be added to the body of your request to the Send API
-// 		response = {
-// 			"text": `You sent the message: '${receivedMessage.text}'. Now send me an attachment!`
-// 		};
-// 	} else if (receivedMessage.attachments) {
-
-// 		// Get the URL of the message attachment
-// 		let attachmentUrl = receivedMessage.attachments[0].payload.url;
-// 		response = {
-// 			"attachment": {
-// 				"type": "template",
-// 				"payload": {
-// 					"template_type": "generic",
-// 					"elements": [{
-// 						"title": "Is this the right picture?",
-// 						"subtitle": "Tap a button to answer.",
-// 						"image_url": attachmentUrl,
-// 						"buttons": [
-// 							{
-// 								"type": "postback",
-// 								"title": "Yes!",
-// 								"payload": "yes",
-// 							},
-// 							{
-// 								"type": "postback",
-// 								"title": "No!",
-// 								"payload": "no",
-// 							}
-// 						],
-// 					}]
-// 				}
-// 			}
-// 		};
-// 	}
-
-// 	// Send the response message
-// 	callSendAPI(senderPsid, response);
-// }
-
-// // Handles messaging_postbacks events
-// function handlePostback(senderPsid, receivedPostback) {
-// 	let response;
-
-// 	// Get the payload for the postback
-// 	let payload = receivedPostback.payload;
-
-// 	// Set the response based on the postback payload
-// 	if (payload === "yes") {
-// 		response = { "text": "Thanks!" };
-// 	} else if (payload === "no") {
-// 		response = { "text": "Oops, try sending another image." };
-// 	}
-// 	// Send the message to acknowledge the postback
-// 	callSendAPI(senderPsid, response);
-// }
-
-// // Sends response messages via the Send API
-// function callSendAPI(senderPsid, response) {
-
-// 	// The page access token we have generated in your app settings
-// 	const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-
-// 	// Construct the message body
-// 	let requestBody = {
-// 		"recipient": {
-// 			"id": senderPsid
-// 		},
-// 		"message": response
-// 	};
-
-// 	// Send the HTTP request to the Messenger Platform
-// 	request({
-// 		"uri": "https://graph.facebook.com/v2.6/me/messages",
-// 		"qs": { "access_token": PAGE_ACCESS_TOKEN },
-// 		"method": "POST",
-// 		"json": requestBody
-// 	}, (err, _res, _body) => {
-// 		if (!err) {
-// 			console.log("Message sent!");
-// 		} else {
-// 			console.error("Unable to send message:" + err);
-// 		}
-// 	});
-// }
 
 async function sendReply(psid, message) {
 	const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
@@ -102,6 +10,21 @@ async function sendReply(psid, message) {
 		},
 		"message": message
 	};
+
+	// request({
+	// 	"uri": "https://graph.facebook.com/v2.6/me/messages",
+	// 	"qs": { "access_token": PAGE_ACCESS_TOKEN },
+	// 	"method": "POST",
+	// 	"json": requestBody
+	// }, (err, _res, _body) => {
+	// 	if (!err) {
+	// 		console.log("Message sent!");
+	// 		console.log(_body);
+	// 	} else {
+	// 		console.error("Unable to send message:" + err);
+	// 	}
+	// });
+	// console.log(requestBody);
 
 	return axios.post(
 		"https://graph.facebook.com/v2.6/me/messages",

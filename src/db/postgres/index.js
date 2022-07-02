99ed8fdf81f8d11f1a450b1ddef88602d9367d4c
Naufal Prima Yoriko <primayoriko@gmail.com>
2022-07-02 23:21:15 +0700

    fix: save psid as bigint in postgres

--- src/db/postgres/index.js
+++ src/db/postgres/index.js
@@ -1,5 +1,10 @@
 const 
-	{ Pool } = require("pg"),
+	{ Pool, types } = require("pg"),
 	pool = new Pool();
+
+// parse bigint as number in js 
+types.setTypeParser(20, function(val) {
+	return parseInt(val, 10);
+});
 
 module.exports = pool;
--- src/migrations/postgres/up.js
+++ src/migrations/postgres/up.js
@@ -11,14 +11,14 @@ async function migrateDB() {
 	const 
 		createUsersTableQuery = 
             `CREATE TABLE IF NOT EXISTS users (
-                psid integer NOT NULL UNIQUE,
+                psid BIGINT NOT NULL UNIQUE,
                 name varchar(255) NOT NULL,
                 PRIMARY KEY(psid)
             )`,
 		createMessagesTableQuery = 
             `CREATE TABLE IF NOT EXISTS messages (
                 id serial PRIMARY KEY,
-                user_psid integer NOT NULL,
+                user_psid BIGINT NOT NULL,
                 message_text TEXT NOT NULL,
                 created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (user_psid) REFERENCES users(psid) ON DELETE CASCADE ON UPDATE CASCADE
